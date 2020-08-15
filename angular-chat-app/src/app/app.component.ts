36bb2095fbda892970dd3985c42910e7e3c37ba6
hardikshakya <hardikshakya1997@gmail.com>
2020-08-15 14:41:42 +0530

    P1T2: Installed and Configured Firebase SDK

--- angular-chat-app/src/app/app.component.ts
+++ angular-chat-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'YOUR_API_KEY',
+  databaseURL: 'YOUR_DATABASE_URL',
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-chat-app';
+
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
